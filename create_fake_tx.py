669ccd289df362830ac4c6cc355ab5c490fc424d
kuro1 <412681778@qq.com>
2017-10-19 11:08:28 +0800

    test case

--- create_fake_tx.py
+++ create_fake_tx.py
@@ -6,22 +6,23 @@ from bigchaindb import Bigchain
 
 delay = 4
 
+
 def create_transfer():
     ##################################################### 1.CREATE
     # Cryptographic Identities Generation
     alice, bob = generate_keypair(), generate_keypair()
-    
+
     # Digital Asset Definition (e.g. bicycle)
-    asset = Asset(data={"bicycle": {"manufacturer":  "bkfab" ,"serial_number":  "abcd1234"}})
+    asset = Asset(data={"bicycle": {"manufacturer": "bkfab", "serial_number": "abcd1234"}})
 
     # Metadata Definition
     metadata = {'planet': 'earth'}
 
     # create trnsaction  TODO : owners_before might be node_pubkey in v0.8.0
-    tx = Transaction.create([alice.public_key], [([alice.public_key],1)], metadata = metadata, asset = asset)
+    tx = Transaction.create([alice.public_key], [([alice.public_key], 1)], metadata=metadata, asset=asset)
     print(" ")
-    print("1.tx_create asset id    :  alice-----bicycle(",tx.to_dict()['transaction']['asset']['id'],")----->alice")
-    print("1.tx_create tx id       : ",tx.to_dict()['id'])
+    print("1.tx_create asset id    :  alice-----bicycle(", tx.to_dict()['transaction']['asset']['id'], ")----->alice")
+    print("1.tx_create tx id       : ", tx.to_dict()['id'])
 
     # sign with alice's private key
     tx = tx.sign([alice.private_key])
@@ -29,34 +30,34 @@ def create_transfer():
 
     # write to backlog
     b = Bigchain()
-    print("1.tx_create db response : ",b.write_transaction(tx))
+    print("1.tx_create db response : ", b.write_transaction(tx))
 
     # wait 2 sec
     sleep(delay)
 
     # get tx by id
     tx = b.get_transaction(tx_id)
-    print("1.tx_create query       : ",tx)
+    print("1.tx_create query       : ", tx)
     # print("tx1_id:"+tx.to_dict()['id'])
     print(" ")
     ##################################################### 2.TRANSFER
     #  inputs and asset [fake]
     cid = 0
     condition = tx.to_dict()['transaction']['conditions'][cid]
-    inputs =Fulfillment.from_dict({
+    inputs = Fulfillment.from_dict({
         'fulfillment': condition['condition']['details'],
         'input': {
             'cid': cid,
             'txid': tx.to_dict()['id'],
-         },
-         'owners_before': [bob.public_key],
+        },
+        'owners_before': [bob.public_key],
     })
     asset = Asset.from_dict(tx.to_dict()['transaction']['asset'])
 
     # transfer
-    tx = Transaction.transfer([inputs], [([bob.public_key],1)],asset)
-    print("2.tx_fake asset id    :  bob-----bicycle(",tx.to_dict()['transaction']['asset']['id'],")----->bob")
-    print("2.tx_fake tx id       : ",tx.to_dict()['id'])
+    tx = Transaction.transfer([inputs], [([bob.public_key], 1)], asset)
+    print("2.tx_fake asset id    :  bob-----bicycle(", tx.to_dict()['transaction']['asset']['id'], ")----->bob")
+    print("2.tx_fake tx id       : ", tx.to_dict()['id'])
 
     # sign with bob's private key [fake]
     tx = tx.sign([bob.private_key])
@@ -64,16 +65,17 @@ def create_transfer():
 
     # write to backlog
     b = Bigchain()
-    print("2.tx_fake db response : ",b.write_transaction(tx))
+    print("2.tx_fake db response : ", b.write_transaction(tx))
 
     # wait 2 sec
     sleep(delay)
 
     # get tx by id
     tx = b.get_transaction(tx_id)
-    print("2.tx_fake query       : ",tx)
-    #print("tx2_id:"+tx.to_dict()['id'])
+    print("2.tx_fake query       : ", tx)
+    # print("tx2_id:"+tx.to_dict()['id'])
     print(" ")
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     create_transfer()
-    
