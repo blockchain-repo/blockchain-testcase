70abbc955f8147d563f7938d8531c780ff8fb140
kuro1 <412681778@qq.com>
2017-10-26 10:22:32 +0800

    tamper_amount 1000---->300 1000---->600

---
+++ tamper_amount.py
@@ -0,0 +1,83 @@
+from time import sleep
+from crypto import generate_keypair
+
+from bigchaindb.common.transaction import Transaction, Asset, Fulfillment
+from bigchaindb.common.util import serialize
+from bigchaindb import Bigchain
+
+delay = 4
+
+
+def create_transfer():
+    ##################################################### 1.CREATE
+    # Cryptographic Identities Generation
+    alice, bob, tom = generate_keypair(), generate_keypair(), generate_keypair()
+    asset = Asset(data={'money': 'RMB'}, data_id='20170628150000', divisible=True)
+    metadata = {'planet': 'earth'}
+
+    tx = Transaction.create([alice.public_key], [([alice.public_key], 1000)], metadata=metadata, asset=asset)
+    print(" ")
+    print("1.tx_create asset id    :  alice-----money 1000(", tx.to_dict()['transaction']['asset']['id'],
+          ")----->alice")
+    print("1.tx_create tx id       : ", tx.to_dict()['id'])
+
+    # sign with alice's private key
+    tx = tx.sign([alice.private_key])
+    tx_id = tx.to_dict()['id']
+
+    # write to backlog
+    b = Bigchain()
+    print("1.tx_create db response : ", b.write_transaction(tx))
+
+    # wait 4 sec
+    sleep(delay)
+
+    # get tx by id
+    tx = b.get_transaction(tx_id)
+    print("1.tx_create query       : ", tx)
+    # print("tx1_id:"+tx.to_dict()['id'])
+    print(" ")
+    ##################################################### 2.TRANSFER
+    #  inputs and asset
+    cid = 0
+    condition = tx.to_dict()['transaction']['conditions'][cid]
+    inputs = Fulfillment.from_dict({
+        'fulfillment': condition['condition']['details'],
+        'input': {
+            'cid': cid,
+            'txid': tx.to_dict()['id'],
+        },
+        'owners_before': condition['owners_after'],
+    })
+    asset = Asset.from_dict(tx.to_dict()['transaction']['asset'])
+
+    # transfer
+    tx = Transaction.transfer([inputs], [([bob.public_key], 300), ([alice.public_key], 700)], asset)
+    print("2.tx_transfer asset id    :  alice-----money 300(", tx.to_dict()['transaction']['asset']['id'], ")----->bob")
+    print("2.tx_transfer tx id       : ", tx.to_dict()['id'])
+    print("2.tx_transfer tx          : ", tx.to_dict())
+    print(" ")
+    # sign with alice's private key
+    tx = tx.sign([alice.private_key])
+
+    # man in the middle attack, then write to backlog
+    tx.conditions[0].amount = 600
+    tx.conditions[1].amount = 400
+    print("3.tx_attack asset id    :  alice-----money 600(", tx.to_dict()['transaction']['asset']['id'], ")----->tom")
+    print("3.tx_attack tx id       : ", tx.to_dict()['id'])
+    print("3.tx_attack tx          : ", tx.to_dict())
+    tx_id = tx.to_dict()['id']
+    b = Bigchain()
+    print("3.tx_attack db response : ", b.write_transaction(tx))
+
+    # wait 4 sec
+    sleep(delay)
+
+    # get tx by id
+    tx = b.get_transaction(tx_id)
+    print("3.tx_attack query       : ", tx)
+    print(" ")
+
+
+if __name__ == '__main__':
+    create_transfer()
